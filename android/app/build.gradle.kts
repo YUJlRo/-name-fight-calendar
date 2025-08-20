97cc819d85b231a67132fdd4ca08152df7549929
YUJlRo <166096105+YUJlRo@users.noreply.github.com>
2025-08-21 04:26:05 +0900

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -118,4 +118,21 @@ dependencies {
     
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+defaultConfig {
+    minSdk = 26
+    targetSdk = 34
+    versionCode = 1
+    versionName = "0.9.0"
+}
+buildTypes {
+    getByName("release") { isMinifyEnabled = false }
+}
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlinOptions { jvmTarget = "17" }
+
 }
