2c13d56ca60b5b14a64e2c50a3fcc0418fe16112
YUJlRo <166096105+YUJlRo@users.noreply.github.com>
2025-08-21 09:41:38 +0900

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,36 +1,4 @@
 plugins {
-    id("com.android.application")
-}
-
-android {
-    namespace = "com.fightcalendar.app"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.fightcalendar.app"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "0.9.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        // Javaのみでビルド
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
